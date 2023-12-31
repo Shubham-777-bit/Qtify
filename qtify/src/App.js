3b097c10ad44ae8ad5ae53118724eeab9fdae86b
Shubham Lalwani <lalwanishubham171@gmail.com>
2023-12-31 15:48:49 +0530

    hero-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import HeroSection from './components/HeroSection/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <HeroSection />
     </div>
   );
 }
